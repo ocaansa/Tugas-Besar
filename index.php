39db6ae5531ab10efaa830d42a27929be2faa10a
arya <123950149+Arya121503@users.noreply.github.com>
2023-12-12 22:43:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+
+
+
+// Path: index.php
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <!-- header -->
+        <div class="nav-container">
+            <header>
+                <label for="hamburger">&#9776;</label>
+                <input type="checkbox" id="hamburger">
+                <div class="logo">
+                    <h1>logo</h1>
+                    <a href=""><img src="" alt="logo"></a>
+                </div>
+                <div class="search-container">
+                    <input type="text" placeholder="Search">
+                    <i class="fa fa-search"></i>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="index.php">About</a></li>
+                        <li><a href="index.php">Medi-Care</a></li>
+                        <li><a href="index.php">Login</a></li>
+                    </ul>
+            </header>
+        </div>
+    </section>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/">
+    <title>Halaman Login</title>
+</head>
+<body>
+    <h1>Sign Up</h1>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="username" autocomplete="off">
+            </li>
+            <li>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
