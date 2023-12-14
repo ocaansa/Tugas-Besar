86c48c843dae8e3729939e652b8386e74b89491e
arya <123950149+Arya121503@users.noreply.github.com>
2023-12-14 22:49:04 +0700

    menambahkan file baru

--- index.php
+++ index.php
@@ -1,54 +1,79 @@
 <?php
 
-
-
-
-
-
-// Path: index.php
 ?>
-
-
-
 
 
 <!-- HTML -->
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/index.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/index.css">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Pet Love</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
-    <section>
-        <!-- header -->
-        <div class="nav-container">
-            <header>
-                <label for="hamburger">&#9776;</label>
-                <input type="checkbox" id="hamburger">
-                <div class="logo">
-                    <h1>logo</h1>
-                    <a href=""><img src="" alt="logo"></a>
-                </div>
-                <div class="search-container">
-                    <input type="text" placeholder="Search">
-                    <i class="fa fa-search"></i>
-                </div>
-                <nav>
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="index.php">About</a></li>
-                        <li><a href="index.php">Medi-Care</a></li>
-                        <li><a href="index.php">Login</a></li>
-                    </ul>
-            </header>
+    <header>
+        <!-- Emergency Call Button -->
+        <div class="emergency-call">
+            <a href="#" class="btn-emergency">Emergency Call</a>
+        </div>     
+    </header>
+
+    <!-- Main-Header -->
+    <div class="main-header">
+        <a href="#" class="logo">Pet<span>Love</span></a>   
+
+        <ul class="navlist">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Layanan</a></li>
+            <li><a href="#">Artikel</a></li>
+        </ul>
+
+        <div class="icons">
+            <a href="#"><i class='bx bx-search-alt-2'></i></a>
+            <a href="#"><i class='bx bx-cart'></i></a>
+            <!-- <a href="#"><i class='bx bx-phone'></i></a> -->
+            <div class="bx bx-menu" id="menu-icon"></div>
+        </div>
+    </div>
+
+    <section class="main-body">
+        <div class="main-text">
+            <h5>Welcome!</h5>
+            <h1>Clinic for Your <br> <span>Best Friends</span> </h1>
+            <br>
+            <!-- <p>Choose the service, <br> now they love.</p> -->
+            <div class="main-btn">
+                <a href="#" class="btn">Masuk</a>
+                <a href="#" class="btn btn2">Daftar</a>
+            </div>
+            
         </div>
     </section>
+
+    <!-- <div class="left">
+        <a href="#"><i class='bx bxl-twitter'></i></a>
+        <a href="#"><i class='bx bxl-instagram'></i></a>
+        <a href="#"><i class='bx bxl-facebook'></i></a>
+    </div> -->
+
+    <div class="right">
+        <a href="#" class="scroll">Scroll Down<i class='bx bx-chevrons-down'></i></a>
+    </div>
+
+    <!----custom js link--->
+    <script type="text/javascript" src="js/script.js"></script>
+
 </body>
+
 </html>
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// index.php
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+}
